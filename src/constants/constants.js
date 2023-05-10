906fa11582bee10b97a03cd3ddb9c4c7b0973848
Roman Broshnitskiy <romario777v8@gmail.com>
2023-05-11 00:43:25 +0300

    add constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,11 @@
 export const constants = {
   colors: {
-    $styleColor: '#28A745',
+    $styleColor: '#F59E0B',
+    $green: '#10B981',
+    $lightgreen: '#D1FAE5',
     $white: '#FFFFFF',
-    $textColor: '#333333',
-    $placeholderColor: '#808080',
+    $lightGrey: '#888B97',
+    $textColor: '#626687',
+    $textHeaderColor: '#1B1C57',
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalStyles } from './components/GlobalStyles';
+import { GlobalStyles } from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import { constants } from './constants/constants';
 import { App } from 'components/App';
 import 'modern-normalize/modern-normalize.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
